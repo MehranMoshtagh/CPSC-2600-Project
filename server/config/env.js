2cdfd4688e1f96913ff8a4a03c66092dc26c304d
Mehran Moshtagh Khorasani <77103716+MehranMoshtagh@users.noreply.github.com>
2025-04-07 02:24:24 -0700

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 443,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/eventManagement',
 };
