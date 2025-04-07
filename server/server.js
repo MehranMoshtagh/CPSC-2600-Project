3a453a52f1bfce02a33d0533021ef201e15c676d
Mehran Moshtagh Khorasani <77103716+MehranMoshtagh@users.noreply.github.com>
2025-04-07 02:30:24 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const path = require("path");
 
 const { connectToDatabase } = require('./config/database');
 const apiRoutes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
-const path = require("path");
 
 // Enable CORS
 app.use(cors());
@@ -16,16 +16,18 @@ app.use(express.json());
 // Mount API routes under /api/v1
 app.use('/api/v1', apiRoutes);
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, "public/build")));
 
+// Serve index.html for all other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "public/build", "index.html"));
 });
 
 // Global error handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 
 async function startServer() {
   try {
